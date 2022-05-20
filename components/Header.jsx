094bb8eddf82af84f9f0c8fcda76ee8fcd864b0f
EGold <61056494+esgoldschmidt@users.noreply.github.com>
2022-05-20 11:45:53 -0400

    header include company name

--- components/Header.jsx
+++ components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
                 <div className='md:float-left block'>
                     <Link href='/'>
                         <span className='cursor-pointer font-bold text-4xl text-gray-400'>
-                            At a Glimpse <span className='font-normal text-3xl'>| Blog </span>
+                            At a Glimpse <span className='font-normal text-xl'>| The Glimpse Group Blog </span>
                         </span>
                     </Link>
                 </div>
