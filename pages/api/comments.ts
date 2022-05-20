52774e408ca597f98f8a3e45deeb6c59a638c824
EGold <61056494+esgoldschmidt@users.noreply.github.com>
2022-05-19 22:24:14 -0400

    working though deployment 5

--- pages/api/comments.ts
+++ pages/api/comments.ts
@@ -5,7 +5,6 @@
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import type { NextApiRequest, NextApiResponse } from 'next';
 import { GraphQLClient, gql } from 'graphql-request';
 
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
