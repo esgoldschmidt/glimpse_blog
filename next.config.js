e94641ec2e605917d88e3b84c70f9a186063dacf
EGold <61056494+esgoldschmidt@users.noreply.github.com>
2022-06-30 15:09:26 -0400

    config

--- next.config.js
+++ next.config.js
@@ -4,9 +4,9 @@ module.exports = {
   future: {
     webpack5: true
   },
-  webpack: (config) => {
+  webpack: (config, options) => {
     // load worker files as a urls with `file-loader`
-    config.module.rules.unshift({
+    config.module.rules.push({
       test: /pdf\.worker\.(min\.)?js/,
       use: [
         {
@@ -16,11 +16,11 @@ module.exports = {
             publicPath: "_next/static/worker",
             outputPath: "static/worker"
           }
-        }
-      ]
+        },
+      ],
     })
 
     return config;
-  }
+  },
 
 }
